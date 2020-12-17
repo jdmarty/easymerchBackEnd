8ce1a2a90ffe17b9b1dfb64dfc124f650cbef64e
JD Marty <joshua.dmarty@gmail.com>
2020-12-16 19:06:29 -0800

    fixed naming bug in associations

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'cateogry_id'
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
@@ -20,7 +20,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  as: "product_tag"
+  as: "tagged_product"
 })
 
 // Tags belongToMany Products (through ProductTag)
@@ -29,7 +29,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: "tagged_product"
+  as: "tag_for_product"
 })
 
 
