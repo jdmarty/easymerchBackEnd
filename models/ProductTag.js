a3b3ae48ef03c252b588d899cbed3ceea34a566f
JD Marty <joshua.dmarty@gmail.com>
2020-12-16 18:41:37 -0800

    added columns to ProducTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,14 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+        unique: false,
+      }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
