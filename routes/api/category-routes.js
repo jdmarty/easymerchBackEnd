fbb8cf053533e3f5d31e08a05222fbd47460439f
JD Marty <joshua.dmarty@gmail.com>
2020-12-16 20:12:49 -0800

    added route to update category names by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,6 +9,7 @@ router.get('/', async (req, res) => {
   try {
     const categoriesData = await Category.findAll({
       include: { model: Product },
+      order: ['id']
     })
     res.status(200).json(categoriesData);
   } catch (err) {
@@ -44,8 +45,25 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: { id: req.params.id }
+      },
+    )
+    if (!updatedCategory) {
+      res.status(404).json({ message: "No category found with this id!" });
+    }
+    console.log(`Category name updated!`);
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
