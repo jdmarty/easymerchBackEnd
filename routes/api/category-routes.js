de60a615157e0e4623e7b5795597976c21082399
JD Marty <joshua.dmarty@gmail.com>
2020-12-16 19:40:57 -0800

    added route to get a single category and associated products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoriesData = await Category.findAll({
-      include: { model: Product }
+      include: { model: Product },
     })
     res.status(200).json(categoriesData);
   } catch (err) {
@@ -21,8 +21,13 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      
-    })
+      include: { model: Product },
+    });
+    //if no data is returned, send a 404 error
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+    }
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
